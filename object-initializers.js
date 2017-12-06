8d88b81642e31bba1f80f734e019c587cd935f47
Brent Guistwite <BrentGuistwite@gmail.com>
2017-12-06 14:18:13 -0500

    added first drill

---
+++ object-initializers.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function(){
+    return this.water / this.flour * 100;
+
+  }    
+};
+
+console.log(loaf.flour);
+
+//Fill in the body of the method to return the hydration of the loaf 
+//(the water divided by the flour multiplied by 100).
+//Call the hydration method and use console.log to print the result.
