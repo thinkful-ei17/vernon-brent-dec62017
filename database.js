6df17836cc50c8728842f1f280a6a2438478eaf6
Vernon Mensah <vernonmensah@gmail.com>
2017-12-06 17:50:32 -0500

    added databae.js

---
+++ database.js
@@ -0,0 +1,20 @@
+const Database = {
+  findOne: function (heroId){
+    return this.store.heroes.find(function (hero) {
+      return hero.id === heroId;
+
+    });
+  },
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  }
+};
+console.log(Database.findOne(2));
--- factoryFunc.js
+++ factoryFunc.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-function createCharacter (name, nickname, race, origin, attack, defense) {
+function createCharacter (name, nickname, race, origin, attack, defense, weapon) {
   return {
     "name": `${name}`,
     "nickname": `${nickname}`,
@@ -10,7 +10,7 @@ function createCharacter (name, nickname, race, origin, attack, defense) {
     "attack": `${attack}`,
     "defense": `${defense}`,
     describe: function () {
-      return `${name} is a ${race} from ${origin}.`;
+      return `${name} is a ${race} from ${origin} who uses ${weapon}`;
     },
     evaluateFight: function (obj) {
       let dmgGiven = 0;
@@ -37,24 +37,30 @@ function createCharacter (name, nickname, race, origin, attack, defense) {
 const first = createCharacter("Gandalf the White","gandalf","Wizard","Middle Earth",10,6);
 const second = createCharacter("Rhonda the Black","rhonda","Witch","Middle Earth",1000,600);
 const characters = [
-  createCharacter("Gandalf the White","gandalf","Wizard","Middle Earth",10,6),
-  createCharacter("Bilbo Baggings", "bilbo", "Hobbit", "The Shire" ,2,1 ),
-  createCharacter("Frodo Baggings", "frodo", "Hobbit", "The Shire" ,3,2 ),
-  createCharacter("Aragorn son of Arathorn", "aragorn", "Man", "Dunnedain" ,6,8 ),
-  createCharacter("Legolas", "legolas", "Elf", "Woodland Realm" ,8, 5),
-  createCharacter("Arwen Undomiel", "arwen", "Half-Elf", "Rivendell", 500,500);
+  createCharacter("Gandalf the White","gandalf","Wizard","Middle Earth",10,6, "wizard staff"),
+  createCharacter("Bilbo Baggings", "bilbo", "Hobbit", "The Shire" ,2,1,"THE RING" ),
+  createCharacter("Frodo Baggings", "frodo", "Hobbit", "The Shire" ,3,2, "String Barrowblade"),
+  createCharacter("Aragorn son of Arathorn", "aragorn", "Man", "Dunnedain" ,6,8, "Anduril"),
+  createCharacter("Legolas", "legolas", "Elf", "Woodland Realm" ,8, 5, "Bow and Arrow"),
+  createCharacter("Arwen Undomiel", "arwen", "Half-Elf", "Rivendell", 500,500, "Hadhafang")
 ];
 
-console.log(characters);
+//console.log(characters);
 
-let newObj = characters.find() {
+let newObj = characters.find(function (n){
+    return n.nickname === "aragorn";
+});
 
-};
 
-let newArr = characters.filter() {
+//console.log(newObj.describe());
 
-};
+let hobbits = characters.filter(function (n) {
+  return n.race === "Hobbit";
+});
 
-let newArr = characters.filter() {
+//console.log(hobbits);
+let strongAttackers = characters.filter(function (n) {
+  return n.attack > 5;
+})
 
-};
+console.log(strongAttackers);
