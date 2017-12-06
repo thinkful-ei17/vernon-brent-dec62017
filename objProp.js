0de4c5c8836fbae4dd052e220ad21c3ce4ebb27b
Vernon Mensah <vernonmensah@gmail.com>
2017-12-06 14:29:38 -0500

    Added looping over object props js

---
+++ objProp.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Create an object with five properties: foo, bar, fum, quux, and spam.
+// Give each property a unique value of your choosing.
+// Loop over the object using for ... in
+// Use console.log to show each property name and its associated value.
+
+const obj = {
+  "foo": 3,
+  "bar": 4,
+  "fum": 5,
+  "quux": 6,
+  "spam": 8,
+
+};
+
+for (let x in obj) {
+
+  console.log(`Name: ${x} - Prop: ${obj[x]}`);
+}
--- object-initializers.js
+++ object-initializers.js
@@ -6,11 +6,11 @@ const loaf = {
   hydration: function(){
     return this.water / this.flour * 100;
 
-  }    
+  }
 };
 
-console.log(loaf.flour);
+console.log(loaf.hydration());
 
-//Fill in the body of the method to return the hydration of the loaf 
+//Fill in the body of the method to return the hydration of the loaf
 //(the water divided by the flour multiplied by 100).
 //Call the hydration method and use console.log to print the result.
